96c8908e21de0a0c8cbae77533852a327a90dc7d
abdel karim dawoud <dawoudria@gmail.com>
2024-06-24 09:26:41 +0300

    add getProfile functionality

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -148,4 +148,20 @@ const updateUser=async(req,res)=>{
     }
 }
 
-export { signupUser, loginUser, logoutUser, followAndunfollowUser, updateUser}; 
+const getUserProfile= async(req,res)=>{
+    const {username}=req.params;
+    try {
+        const user = await User.findOne({username}).select("-password").select("-updatedAt");
+        if(!user) return res.status(400).json({message:"user not found"});
+
+        res.status(200).json(user);
+
+        
+        
+    } catch (error) {
+        res.status(500).json({message:error.message});
+        console.log("error in get user profile",error.message);        
+    }
+}
+
+export { signupUser, loginUser, logoutUser, followAndunfollowUser, updateUser, getUserProfile}; 
