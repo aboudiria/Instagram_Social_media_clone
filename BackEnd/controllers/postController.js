d89bf37af2f1cd60ea752bed295fffd60742ed82
abdel karim dawoud <dawoudria@gmail.com>
2024-06-26 15:34:39 +0300

    add get user post functionality

--- BackEnd/controllers/postController.js
+++ BackEnd/controllers/postController.js
@@ -133,5 +133,40 @@ const replyToPost=async(req,res)=>{
 }
 }
 
+const getFeedPosts = async (req, res) => {
+	try {
+		const userId = req.user._id;
+		const user = await User.findById(userId);
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
 
-export {createPost, getPost , deletePost, likeUnlikePost,replyToPost};
+		const following = user.following;
+
+		const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({ createdAt: -1 });
+
+		res.status(200).json(feedPosts);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+const getUserPosts = async (req, res) => {
+	const { username } = req.params;
+	try {
+		const user = await User.findOne({ username });
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		const posts = await Post.find({ postedBy: user._id }).sort({ createdAt: -1 });
+
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+
+
+export {createPost, getPost , deletePost, likeUnlikePost, replyToPost, getFeedPosts, getUserPosts};
